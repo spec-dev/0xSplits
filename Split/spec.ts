99375f35c7b7bb43098a78996296f80b4fec7d81
Ben Whittle <benwhittle31@gmail.com>
2023-07-26 04:49:33 -0500

    add split update and contol-transfer handling

--- Split/spec.ts
+++ Split/spec.ts
@@ -1,4 +1,4 @@
-import { LiveObject, Spec, Property, Call, OnCall, Address, BigInt } from '@spec.dev/core'
+import { LiveObject, Spec, Property, Call, OnCall, Event, OnEvent, Address, BigInt, Timestamp, ZERO_ADDRESS } from '@spec.dev/core'
 
 /**
  * A split contract on the 0xSplits protocol.
@@ -19,6 +19,14 @@ class Split extends LiveObject {
     @Property()
     controller: Address
 
+    // Controller assumed during transfer.
+    @Property({ default: ZERO_ADDRESS })
+    newPotentialController: Address
+
+    // When the split was created.
+    @Property()
+    createdAt: Timestamp
+
     // ==== Call Handlers ===================
 
     @OnCall('0xsplits.SplitMain.createSplit')
@@ -26,6 +34,29 @@ class Split extends LiveObject {
         this.address = call.outputs.split
         this.distributorFee = BigInt.from(call.inputs.distributorFee)
         this.controller = call.inputs.controller
+        this.createdAt = this.blockTimestamp
+    }
+
+    @OnCall('0xsplits.SplitMain.updateSplit')
+    onUpdateSplit(call: Call) {
+        this.address = call.outputs.split
+        this.distributorFee = BigInt.from(call.inputs.distributorFee)
+    }
+
+    // ==== Event Handlers ==================
+
+    @OnEvent('0xsplits.SplitMain.InitiateControlTransfer')
+    onInitiateControlTransfer(event: Event) {
+        this.address = event.data.split
+        this.newPotentialController = event.data.newPotentialController
+    }
+
+    @OnEvent('0xsplits.SplitMain.ControlTransfer')
+    async onControlTransfer(event: Event) {
+        this.address = event.data.split
+        if (!(await this.load())) return false
+        this.controller = event.data.newController
+        this.newPotentialController = ZERO_ADDRESS
     }
 }
 
